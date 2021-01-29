47d5c03576ddeeba42b21d45c5c002da64f009b9
Poonam Hiwal <pooyadav@users.noreply.github.com>
2021-01-29 09:28:06 +0000

    Update getperf.sh

--- LAB1_v1/getperf.sh
+++ LAB1_v1/getperf.sh
@@ -11,38 +11,3 @@ while [ $COUNT -le $MAX ]; do
 	let COUNT=COUNT+STEP
 done
 
-input="./sample_data.txt"
-while IFS= read -r line
-do
-  let rem=($LineCount%2)
-  newline=$(echo $line | tr -d  s) 
-  matrix+=($newline)
-  if [ $rem -eq 1 ]; then
-     writematrix+=(${matrix[1]})
-  else
-   writematrix+=(${matrix[1]})
-   writematrix+=(${matrix[2]})
-   writematrix+=(${matrix[4]})
-   writematrix+=(${matrix[6]})
-   echo ${writematrix[0]}","${writematrix[1]}","${writematrix[2]}","${writematrix[3]}","${writematrix[4]} >> processed.csv
-   writematrix=()
-  fi 
-  matrix=()
-  let LineCount=LineCount+1
-done < "$input"
-
-#! code for ploting data
-for FILE in sample_processed.csv; do
-gnuplot -p << EOF
-set datafile separator ","
-set xlabel "matrix size"
-set xrange [10:$MAX]
-set ylabel "program time"
-set title "Execution time performance"
-plot "$FILE" using 1:2 title "program time", \
-     "$FILE" using 1:3 title "real time",\
-     "$FILE" using 1:4 title "usr time",\
-     "$FILE" using 1:5 title "sys time"
-
-EOF
-done
